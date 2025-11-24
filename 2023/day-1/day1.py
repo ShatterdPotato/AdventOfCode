1c7f998a17d72a25f520e3cb54af3e8adcffd329
Sameer Ahmed <sameera22@nycstudents.net>
2025-11-24 10:18:46 -0500

    partial soluion for 2023 day 1 pt2

--- 2023/day-1/day1.py
+++ 2023/day-1/day1.py
@@ -16,40 +16,52 @@ nums = {
 }
 
 def calculate_first_alphanum(str):
-    min_index = len(str) - 1
+    min_index = 999
+    return_tuple = [0, ""]
     for key in nums:
         try:
-            if (str.index(key) < min_index):
-                min_index = str.index(key)
+            if (str.index(nums[key]) < min_index):
+                min_index = str.index(nums[key])
+                return_tuple[1] = key
         except ValueError:
-            print(f"{key} is not present in {str}")
-    return min_index
+            print(end='')  
+    return_tuple[0] = min_index
+    return return_tuple
 
 def calculate_last_alphanum(str):
-    max_index = 0
-    try:
-        for key in nums:
-            if (str.index(key) > max_index):
-                max_index = str.index(key)
-    except ValueError:
-         print(f"{key} is not present in {str}")
-    return max_index
+    return_tuple = [0, ""]
+    max_index = -1
+    for key in nums:
+            try:
+                if (str.index(nums[key]) > max_index):
+                    max_index = str.index(nums[key])
+                    return_tuple[1] = key
+            except ValueError: 
+                print(end='')   
+    return_tuple[0] = max_index
+    return return_tuple
 
 with open("list.txt", "r") as file:
     for str in file:
-        ones_place_stridx = calculate_last_alphanum(str)
-        tenths_place_stridx = calculate_first_alphanum(str)
         for c in range(len(str)):
-            if (c.isnumeric()):
-                tenths_place = int(c)
-                break
-        for c in range(str.__len__()):
-            char = str[str.__len__() - (c + 1)]
+            char = str[c]
             if (char.isnumeric()):
-                ones_place = int(char)
+                str_tuple = calculate_first_alphanum(str)
+                if (str_tuple[0] < c):
+                    tenths_place = int(str_tuple[1])
+                else:
+                    tenths_place = int(char)
                 break
-        
-        
+        for c in range(len(str)):
+            char = str[len(str) - (c + 1)]
+            if (char.isnumeric()):
+                str_tuple = calculate_last_alphanum(str)
+                if (str_tuple[0] > 0):
+                    ones_place = int(str_tuple[1])
+                else:
+                    ones_place = int(char)
+                break
+        print(int(tenths_place * 10 + ones_place))
         sum += int(tenths_place * 10 + ones_place)
 
 print(sum)
