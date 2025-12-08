2a5b1ce6c715d9c2a31a2c36679bf5038b9f2b7c
Sameer Ahmed <sameera22@nycstudents.net>
2025-12-08 10:07:13 -0500

    solution for 2025 day 5 pt 2

--- 2025/day-5/day5.py
+++ 2025/day-5/day5.py
@@ -29,15 +29,31 @@ def part_one():
     return sum
 
 def part_two():
-    ids = []
-    for range in ranges:
-        upper_bound = int(range.split('-')[1])
-        lower_bound = int(range.split('-')[0]) 
-        while lower_bound <= upper_bound:
-            if not lower_bound in ids:
-                ids.append(lower_bound)
-            lower_bound += 1
-        print(f"done with line {ranges.index(range)}")
-    return len(ids)
+    sum = 0
+    sorted_ranges = []
+    for r in ranges:
+        upper_bound = int(r.split('-')[1])
+        lower_bound = int(r.split('-')[0]) 
+        sorted_ranges.append([lower_bound, upper_bound])
+    
+
+    sorted_ranges = sorted(sorted_ranges)
+    i = 1
+    while i < len(sorted_ranges):
+        if sorted_ranges[i][0] >= sorted_ranges[i - 1][0] and sorted_ranges[i][0] <= sorted_ranges[i - 1][1]:
+            if sorted_ranges[i][1] > sorted_ranges[i - 1][1]:
+                sorted_ranges[i - 1][1] = sorted_ranges[i][1]
+            sorted_ranges.pop(i)
+            i -= 1
+        i += 1
+    for merged_range in sorted_ranges:
+        upper_bound = merged_range[1]
+        lower_bound = merged_range[0]
+        sum += (upper_bound - lower_bound) + 1
+    
+    return sum
+
+            
+
 
 print(part_two())
